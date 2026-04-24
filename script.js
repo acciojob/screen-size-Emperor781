f83b05f312fefe18604df88dab3c436cefeab75c
Cyril Cristo Johns <95585437+Emperor781@users.noreply.github.com>
2026-04-24 11:33:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+let width=document.querySelector('.wid');
+let height=document.querySelector('.hei');
+
+window.addEventListener('resize',()=>{
+let w=window.innerWidth;
+let h=window.innerHeight;
+		
+width.innerText=`Width: ${w}`
+height.innerText=`and Height: ${h}`
+})
+
+
+
+
